ce42cb9d33742a85a68abbd7272f57ce37c6f106
Bbbtt04 <2393714045@qq.com>
2023-03-10 13:18:31 +0800

    feat: use antd

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import React, { Component } from 'react'
+import {Button} from 'antd'
+import 'antd/dist/reset.css';
+
 export default function Home() {
   return (
     <>
       <h1>Hello NextJs</h1>
+      <Button type='primary'>Antd Button</Button>
     </>
   )
 }
